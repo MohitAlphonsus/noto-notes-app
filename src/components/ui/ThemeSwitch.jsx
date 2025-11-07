5e3feb1a0e377015ae34f1260c5b69a23b9560d3
Mohit Alphonsus <mohitdalphonso@gmail.com>
2025-11-07 09:25:44 +0530

    theme switch UI updated

--- src/components/ui/ThemeSwitch.jsx
+++ src/components/ui/ThemeSwitch.jsx
@@ -1,10 +1,24 @@
-import { GiMoon, GiSun } from 'react-icons/gi';
+import { useState, useEffect } from "react";
+import { GiMoon, GiSun } from "react-icons/gi";
+import styles from "./ThemeSwitch.module.css";
 
 export default function ThemeSwitch() {
+	const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+	useEffect(() => {
+		document.documentElement.setAttribute("data-theme", theme);
+		localStorage.setItem("theme", theme);
+	}, [theme]);
+
+	const handleThemeSwitch = () => {
+		setTheme(theme === "light" ? "dark" : "light");
+	};
+
 	return (
-		<div>
-			<GiMoon />
-			{/* <GiSun /> */}
+		<div className={styles.theme}>
+			<button onClick={handleThemeSwitch}>
+				{theme === "light" ? <GiMoon /> : <GiSun />}
+			</button>
 		</div>
 	);
 }
